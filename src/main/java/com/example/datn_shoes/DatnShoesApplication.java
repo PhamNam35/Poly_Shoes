1a98b206a93b28192bbc182d897b6bacbf7474a2
PhamNam35 <pnam40249@gmail.com>
2025-05-21 21:54:30 +0700

    namngu

---
+++ src/main/java/com/example/datn_shoes/DatnShoesApplication.java
@@ -0,0 +1,13 @@
+package com.example.datn_shoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatnShoesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatnShoesApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/datn_shoes/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.datn_shoes;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DatnShoesApplication.class);
+    }
+
+}
